1c477fa038c14831913da5d40933096aef6d03e4
Colter Giem <giemcl@s.dcsdk12.org>
2015-09-11 09:50:17 -0600

    without_doubles complete

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,5 +1,23 @@
-module MethodsQuiz
+module MethodsQuiz2
+	def without_doubles(roll1,roll2,no_doubles)
+		if no_doubles
+			if roll1 == roll2
+				if roll1 == 6
+					roll1 = 1
+					roll1 + roll2
+				else
+					roll1 = roll1 + 1
+					roll1 + roll2
+				end
+			else
+				roll1 + roll2
+			end
+		else
+			roll1 + roll2
+		end
+	end
 	
-	
+	private
+
 
 end
